c3a875c8a0902b361a380d308edb52df0b1d7ca9
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-06-15 10:28:16 +0530

    i have hosted and removed localhost

--- server/server.js
+++ server/server.js
@@ -20,6 +20,12 @@ const app = express();
 // Middleware
 // ---------------------------------------------------------------------------
 app.use(cors());
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
